975bc92e7ed7ab595fbcdab3a34f95a5bd1549fe
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-22 00:42:05 +0200

    [TASK] optimization and cleanup

--- 05-software.sh
+++ 05-software.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-# useful system stuff
-pacman -S sudo bash-completion git networkmanager wget xdotool htop
-
-echo edit sudoers
-echo [press any key to continue]
-read
-EDITOR=nano visudo
-
-echo enable multilib in pacman config
-echo [press any key to continue]
+echo
+echo "====================================="
+echo "=== ENABLE MULTILIB IN PACMAN CONFIG
+echo "=== [press any key to continue]
+echo "====================================="
 read
 nano /etc/pacman.conf
+
+# install software
+pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bash-completion \
+	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla \
+	firefox geany gimp git gvfs-smb htop i3 jdk10-openjdk lib32-glu \
+	lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
+	lxappearance lxdm lxterminal nano network-manager-applet networkmanager pavucontrol \
+	pcmanfm playerctl pulseaudio ristretto rofi scrot sudo ttf-bitstream-vera \
+	ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip xdg-user-dirs \
+	xdg-utils xdotool xf86-input-synaptics xf86-video-intel xorg xorg-xinit
+
+echo
+echo "====================================="
+echo "=== EDIT SUDOERS FILE"
+echo "=== [press any key to continue]"
+echo "====================================="
+read
+EDITOR=nano visudo
 
 # aurman
 mkdir /tmp/aurmansetup
@@ -23,20 +36,4 @@ sudo -u nobody makepkg -sri
 cd
 rm -rf /tmp/aurmansetup
 
-# xorg and drivers
-pacman -S xorg-server xorg-apps xf86-video-intel xf86-input-synaptics bluez bluez-utils
-
-# desktop
-pacman -S acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bash-completion \
-	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla \
-	firefox geany gimp git gvfs-smb htop i3 jdk10-openjdk lib32-glu \
-	lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
-	lxappearance lxdm lxterminal nano network-manager-applet networkmanager pavucontrol \
-	pcmanfm playerctl pulseaudio ristretto rofi scrot sudo ttf-bitstream-vera \
-	ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip xdg-user-dirs \
-	xdg-utils xdotool xf86-input-synaptics xf86-video-intel xorg xorg-xinit
-
-aurman -S acroread google-chrome numix-circle-icon-theme-git nvm spotify
-
-# alternatively an Openbox setup
-# pacman -S openbox openbox-themes obconf oblogout lxappearance-obconf-gtk3 lightdm light-locker lightdm-gtk-greeter compton plank stalonetray slock
+sudo -u nobody aurman -S acroread google-chrome numix-circle-icon-theme-git nvm spotify
