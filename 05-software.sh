81eeb72913ad3bb8996ff12466ca47c43aee6823
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-21 21:15:11 +0200

    [TASK] updated to i3 setup

--- 05-software.sh
+++ 05-software.sh
@@ -18,23 +18,16 @@ rm -rf /tmp/aurmansetup
 pacman -S xorg-server xorg-apps xf86-video-intel xf86-input-synaptics bluez bluez-utils
 
 # desktop
-pacman -S openbox openbox-themes obconf oblogout lxappearance-obconf-gtk3 lightdm light-locker lightdm-gtk-greeter compton plank stalonetray slock
-pacman -S geany thunar ristretto evince file-roller nodejs npm blueberry guake lxterminal rofi shutter network-manager-applet variety nitrogen firefox
-yaourt -S --noconfirm google-chrome lxdm-themes franz brightside skippy-xd-git arc-icon-theme gtk-theme-arc-git
+pacman -S acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bash-completion \
+	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller \
+	firefox geany gimp git gvfs-smb htop i3 jdk10-openjdk lib32-glu \
+	lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
+	lxappearance lxdm lxterminal nano network-manager-applet networkmanager pavucontrol \
+	pcmanfm playerctl pulseaudio ristretto rofi scrot sudo ttf-bitstream-vera \
+	ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip xdg-user-dirs \
+	xdg-utils xdotool xf86-input-synaptics xf86-video-intel xorg xorg-xinit
 
-# infinality
-sudo pacman-key -r 962DDE58
-sudo pacman-key -f 962DDE58
-sudo pacman-key --lsign-key 962DDE58
-echo "
-[infinality-bundle]
-Server = http://bohoomil.com/repo/\$arch
-[infinality-bundle-fonts]
-Server = http://bohoomil.com/repo/fonts" >> /etc/pacman.conf
-pacman -S infinality-bundle ibfonts-meta-base ibfonts-meta-extended
+aurman -S acroread google-chrome numix-circle-icon-theme-git nvm spotify
 
-# geany themes
-git clone https://github.com/codebrainz/geany-themes.git /tmp/geany-themes
-mkdir -p ~/.config/geany/colorschemes
-cp /tmp/geany-themes/colorschemes/* ~/.config/geany/colorschemes/
-rm -rf /tmp/geany-themes
+# alternatively an Openbox setup
+# pacman -S openbox openbox-themes obconf oblogout lxappearance-obconf-gtk3 lightdm light-locker lightdm-gtk-greeter compton plank stalonetray slock
