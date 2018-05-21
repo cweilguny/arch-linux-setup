54151bf266ac746e509c6ff122319d3c82dbfbda
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-21 22:29:18 +0200

    [TASK] added filezilla

--- 05-software.sh
+++ 05-software.sh
@@ -28,7 +28,7 @@ pacman -S xorg-server xorg-apps xf86-video-intel xf86-input-synaptics bluez blue
 
 # desktop
 pacman -S acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bash-completion \
-	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller \
+	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla \
 	firefox geany gimp git gvfs-smb htop i3 jdk10-openjdk lib32-glu \
 	lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
 	lxappearance lxdm lxterminal nano network-manager-applet networkmanager pavucontrol \
