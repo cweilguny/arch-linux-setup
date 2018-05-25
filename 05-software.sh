9ccd9916498d8c92c76ca23dbbf6047a1fa5988d
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-25 11:30:17 +0200

    [TASK] added p7zip

--- 05-software.sh
+++ 05-software.sh
@@ -13,10 +13,10 @@ pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-
 	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla fwupd \
 	firefox geany gimp git gnome-keyring gvfs-smb htop i3 jdk10-openjdk libreoffice-fresh \
 	lib32-glu lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
-	libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet networkmanager openssh pavucontrol \
-	pcmanfm playerctl pulseaudio rdesktop ristretto rofi rsync scrot seahorse speedcrunch sudo ttf-bitstream-vera \
-	ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip xdg-user-dirs \
-	xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
+	libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet networkmanager openssh \
+	p7zip pavucontrol pcmanfm playerctl pulseaudio rdesktop ristretto rofi rsync scrot seahorse speedcrunch \
+	sudo ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip \
+	xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
 
 # enable services
 systemctl enable {lxdm,docker,NetworkManager,mysql}
