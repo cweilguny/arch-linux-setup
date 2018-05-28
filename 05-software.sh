ea22f649fed06e4bc51667f0b3845891d81b7877
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-28 10:44:45 +0200

    [TASK] remove acrobat reader

--- 05-software.sh
+++ 05-software.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-echo
-echo "====================================="
-echo "=== ENABLE MULTILIB IN PACMAN CONFIG
-echo "=== [press any key to continue]
-echo "====================================="
-read
-nano /etc/pacman.conf
-
 # install software
 pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-tools bash-completion \
 	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla fwupd \
 	firefox geany gimp git gnome-keyring gvfs-smb htop i3 jdk10-openjdk libreoffice-fresh \
-	lib32-glu lib32-gtk2 lib32-libidn lib32-libxml2 lib32-libxt lib32-mesa \
 	libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet networkmanager openssh \
 	p7zip pavucontrol pcmanfm playerctl pulseaudio rdesktop ristretto rofi rsync scrot seahorse speedcrunch \
 	sudo ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip \
@@ -39,4 +30,4 @@ sudo -u nobody makepkg -sri
 cd
 rm -rf /tmp/aurmansetup
 
-sudo -u nobody aurman -S acroread google-chrome numix-circle-icon-theme-git spotify
+sudo -u nobody aurman -S google-chrome numix-circle-icon-theme-git spotify
