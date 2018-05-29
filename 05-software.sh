593902d293ad14428046443b990faef47d2fe615
Christian Weilguny <netdesk@users.noreply.github.com>
2018-05-29 11:32:16 +0200

    [TASK] added openconnect

--- 05-software.sh
+++ 05-software.sh
@@ -4,10 +4,11 @@
 pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-tools bash-completion \
 	blueman bluez bluez-utils compton dialog docker docker-compose feh file-roller filezilla fwupd \
 	firefox geany gimp git gnome-keyring gvfs-smb htop i3 jdk10-openjdk libreoffice-fresh \
-	libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet networkmanager openssh \
-	p7zip pavucontrol pcmanfm playerctl pulseaudio rdesktop ristretto rofi rsync scrot seahorse speedcrunch \
-	sudo ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety wget xbindkeys xclip \
-	xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
+	libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet networkmanager \
+	networkmanager-openconnect openssh p7zip pavucontrol pcmanfm playerctl pulseaudio rdesktop \
+	ristretto rofi rsync scrot seahorse speedcrunch sudo ttf-bitstream-vera ttf-dejavu \
+	ttf-liberation tumbler unzip variety wget xbindkeys xclip xdg-user-dirs xdg-utils xdotool \
+	xf86-input-libinput xf86-video-intel xorg xorg-xinit
 
 # enable services
 systemctl enable {lxdm,docker,NetworkManager,mysql}
