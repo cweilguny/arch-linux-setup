f8dd11cd5e6b0eb34ba86bf5cd002c8d3162331b
Christian Weilguny <christian@weilguny.net>
2019-08-10 16:44:58 +0200

    [TASK] add lxrandr

--- 05-software.sh
+++ 05-software.sh
@@ -4,7 +4,7 @@
 pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-tools bash-completion \
 	blueman bluez bluez-utils chromium compton cups dialog docker docker-compose exfat-utils feh file-roller filezilla flameshot fwupd \
 	firefox geany gimp git gnome-disk-utility gnome-keyring gvfs-gphoto2 gvfs-mtp gvfs-smb htop i3 jdk10-openjdk \
-	libreoffice-fresh libsecret lxappearance lxdm lxterminal mysql nano network-manager-applet \
+	libreoffice-fresh libsecret lxappearance lxdm lxrandr lxterminal mysql nano network-manager-applet \
 	networkmanager networkmanager-openconnect openssh p7zip parcellite pavucontrol pulsemixer \
 	playerctl pulseaudio rdesktop ristretto rofi rsync screenfetch scrot seahorse speedcrunch sudo \
 	thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety virtualbox virtualbox-host-modules-arch \
