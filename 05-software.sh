62e5c523591b2bb32278a4db8248d30c258b9e49
c.weilguny <christian@weilguny.net>
2019-11-02 20:23:15 +0000

    [TASK] optimize indents

--- 05-software.sh
+++ 05-software.sh
@@ -2,13 +2,13 @@
 
 # install software
 pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-tools bash-completion \
-	blueman bluez bluez-utils chromium compton cups dialog docker docker-compose exfat-utils feh \
+    blueman bluez bluez-utils chromium compton cups dialog docker docker-compose exfat-utils feh \
     file-roller filezilla flameshot fwupd firefox geany gimp git gnome-disk-utility gnome-keyring \
     gvfs-gphoto2 gvfs-mtp gvfs-smb htop i3 jdk11-openjdk libreoffice-fresh libsecret lxappearance \
     lxdm lxrandr lxterminal nano network-manager-applet networkmanager openssh p7zip parcellite \
     pavucontrol pulsemixer playerctl pulseaudio rdesktop restic ristretto rofi rsync speedcrunch sudo \
-	thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety \
-	wget xbindkeys xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
+    thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety \
+    wget xbindkeys xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
 
 # enable services
 systemctl enable {lxdm,docker,NetworkManager,org.cups.cupsd}
