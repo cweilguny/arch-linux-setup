69b17588df9d7330d99cd32c40fc8dbc8b1d17ef
c.weilguny <christian@weilguny.net>
2019-11-02 17:08:20 +0000

    [TASK] add kernel and firmware

--- 02-install-base.sh
+++ 02-install-base.sh
@@ -2,8 +2,8 @@
 
 # somewhere before you have to do partitioning and mount the root to /mnt and if needed the efi partition to /mnt/boot
 
-pacstrap -i /mnt base base-devel
-genfstab -U /mnt > /mnt/etc/fstab
+pacstrap -i /mnt base base-devel linux linux-firmware intel-ucode
+genfstab -U /mnt >> /mnt/etc/fstab
 
 # copy this scripts to the system so you have them available for execution inside the chroot
 mkdir -p /mnt/tmp/archlinux-setup
