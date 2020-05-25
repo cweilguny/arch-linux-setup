a45ac10c3a20e2998f0f1aac20bc279913d2f1e9
Christian Weilguny <6189441+cweilguny@users.noreply.github.com>
2020-05-25 16:26:44 +0200

    [TASK] changed packages

--- 05-software.sh
+++ 05-software.sh
@@ -5,10 +5,10 @@ pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-
     blueman bluez bluez-utils chromium compton cups dialog docker docker-compose exfat-utils feh \
     file-roller filezilla flameshot fwupd firefox geany gimp git gnome-disk-utility gnome-keyring \
     gvfs-gphoto2 gvfs-mtp gvfs-smb htop i3 jdk11-openjdk libreoffice-fresh libsecret lxappearance \
-    lxdm lxrandr lxterminal nano network-manager-applet networkmanager openssh p7zip parcellite \
+    lxdm lxrandr lxterminal nano network-manager-applet networkmanager numlockx openssh p7zip parcellite \
     pavucontrol pulsemixer playerctl pulseaudio rdesktop restic ristretto rofi rsync speedcrunch sudo \
-    thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation tumbler unzip variety \
-    wget xbindkeys xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
+    thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-roboto ttf-roboto-mono tumbler unzip variety \
+    wget xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
 
 # enable services
 systemctl enable {lxdm,docker,NetworkManager,org.cups.cupsd}
@@ -31,4 +31,4 @@ sudo -u nobody makepkg -sri
 cd
 rm -rf /tmp/yaysetup
 
-sudo -u nobody yay -S systemd-boot-pacman-hook numix-circle-icon-theme-git spotify mirrorlist-manager insync
+sudo -u nobody yay -S systemd-boot-pacman-hook numix-circle-icon-theme-git spotify mirrorlist-manager teams xbindkeys
