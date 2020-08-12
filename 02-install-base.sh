4ebcf9912cba0cef60e3099370a59f35644c24a5
Christian Weilguny <6189441+cweilguny@users.noreply.github.com>
2020-08-12 16:23:40 +0200

    adds networkmanager package on initial installation

--- 02-install-base.sh
+++ 02-install-base.sh
@@ -2,7 +2,7 @@
 
 # somewhere before you have to do partitioning and mount the root to /mnt and if needed the efi partition to /mnt/boot
 
-pacstrap -i /mnt base base-devel linux linux-firmware intel-ucode
+pacstrap -i /mnt base base-devel linux linux-firmware intel-ucode networkmanager
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # copy this scripts to the system so you have them available for execution inside the chroot
