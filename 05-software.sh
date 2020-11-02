17da450a95847b459b7cfe602ab0f38e4d347ae5
Christian Weilguny <6189441+cweilguny@users.noreply.github.com>
2020-11-02 09:41:45 +0100

    adds pulseaudio-modules-bt-git

--- 05-software.sh
+++ 05-software.sh
@@ -8,7 +8,7 @@ pacman -Sy acpi alsa-utils ansible arc-gtk-theme arc-icon-theme autocutsel bind-
     lxdm lxrandr lxterminal nano network-manager-applet networkmanager numlockx openssh p7zip parcellite \
     pavucontrol pulsemixer playerctl pulseaudio rdesktop restic ristretto rofi rsync speedcrunch sudo \
     thunar transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-roboto ttf-roboto-mono tumbler unzip variety \
-    wget xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
+    wget xbindkeys xclip xdg-user-dirs xdg-utils xdotool xf86-input-libinput xf86-video-intel xorg xorg-xinit
 
 # enable services
 systemctl enable {lxdm,docker,NetworkManager,org.cups.cupsd,bluetooth}
@@ -31,4 +31,4 @@ sudo -u nobody makepkg -sri
 cd
 rm -rf /tmp/yaysetup
 
-sudo -u nobody yay -S systemd-boot-pacman-hook numix-circle-icon-theme-git spotify mirrorlist-manager teams xbindkeys
+sudo -u nobody yay -S systemd-boot-pacman-hook numix-circle-icon-theme-git spotify mirrorlist-manager teams pulseaudio-modules-bt-git
